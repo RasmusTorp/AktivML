d83269dcb56e3d792574f5652a9853caa9d89a20
jesperdlau <jesperdlau@gmail.com>
2023-03-23 12:55:29 +0100

    Added model using scikeras

--- model.py
+++ model.py
@@ -21,3 +21,24 @@ def create_model(loss_func, opt, act, input_shape):
 
 SHAPE = (80,80,1)
 Classifier = KerasClassifier(create_model("sparse_categorical_cross_entropy","adam","relu",SHAPE))
+
+
+# Compatibel with the scikeras library
+def get_model(meta):
+    # note that meta is a special argument that will be
+    # handed a dict containing input metadata
+    n_features_in_ = meta["n_features_in_"]
+    X_shape_ = meta["X_shape_"]
+    n_classes_ = meta["n_classes_"]
+
+    model = keras.models.Sequential()
+    model.add(keras.layers.Conv2D(80,(3,3), activation='relu', input_shape=X_shape_[1:]))
+    model.add(keras.layers.MaxPooling2D((2,2), padding='same'))
+    model.add(keras.layers.Conv2D(40,(3,3), activation='relu'))
+    model.add(keras.layers.MaxPooling2D((2,2), padding='same'))
+    model.add(keras.layers.Flatten())
+    model.add(keras.layers.Dense(40))
+    model.add(keras.layers.Activation("relu"))
+    model.add(keras.layers.Dense(n_classes_))
+    model.add(keras.layers.Activation("softmax"))
+    return model
